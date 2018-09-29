51aa806b8516811e92ea2069dd2bc95568c61ec9
12东永 <2808050775@qq.com>
2018-09-29 17:55:23 +0800

    Tutorial: How to set up a Vapor 3 project

--- Hello/Sources/App/configure.swift
+++ Hello/Sources/App/configure.swift
@@ -9,6 +9,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try routes(router)
     services.register(router, as: Router.self)
 
-   
+    ///If you don’t like 8080 it is super easy to change the port of your project. All you need to do is adding the following line to the end of configure.swift:
+    let myService = NIOServerConfig.default(port:8000)
+    services.register(myService)
 
 }
--- Hello/Sources/App/routes.swift
+++ Hello/Sources/App/routes.swift
@@ -2,5 +2,21 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-  
+    ///By using the function get(“name”) we define that this route is reachable if it is requested via the HTTP method GET at the url /name.
+    router.get("name"){req in
+        return "Ethan Hunt"
+    }
+    
+    router.get("age"){req in
+        return 23
+    }
+    
+    router.get("json"){req in
+        return Person(name:"mudy",age:23)
+    }
+    
+    struct Person:Content{
+        var name: String
+        var age : Int
+    }
 }
